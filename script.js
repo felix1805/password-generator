903da19c3ef77a61440395e1020535cccbada5c8
felix1805 <felix1805@gmail.com>
2022-02-28 17:05:07 -0500

    defined character list

--- script.js
+++ script.js
@@ -3,7 +3,17 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   var count = Number(prompt("Please enter the amount of characters you would like in your password."));
   
-  var letters = "abcdefghijklmnopqrstuvwxyz";
+  if (count < 8 || count > 128) { //specified parameters for number of used characters
+    alert("You are not within range, enter at least 8 characters to continue.");
+    return "";
+  }
+  var lowercaseletters = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseletters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbers = "12345678901234567890";
+  var specialchar = "!@#$%^&*()_+=-{}|\\][\":';<>?/.,";
+   //all special case letters applicable for standard keyboard note special case for " and /. 
+  
+  var pool = lowercaseletters;
   var genPassword = "";
 
   for (var i = 0; i < count; i++) {
