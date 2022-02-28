df7897109b7412bdb3ed6f954b1148b940c66c08
felix1805 <felix1805@gmail.com>
2022-02-28 17:26:17 -0500

    added cases for user inputs

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   var count = Number(prompt("Please enter the amount of characters you would like in your password."));
-  
+
   if (count < 8 || count > 128) { //specified parameters for number of used characters
     alert("You are not within range, enter at least 8 characters to continue.");
     return "";
@@ -10,21 +10,27 @@ function generatePassword() {
   var lowercaseletters = "abcdefghijklmnopqrstuvwxyz";
   var uppercaseletters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var numbers = "12345678901234567890";
-  var specialchar = "!@#$%^&*()_+=-{}|\\][\":';<>?/.,";
-   //all special case letters applicable for standard keyboard note special case for " and /. 
+  var specialchar = "!@#$%^&*()_+=-{}|\\][\":';<>?/.,"; //all special case letters applicable for standard keyboard note special case for " and /. 
   
-  var pool = lowercaseletters;
-  var genPassword = "";
-
-  for (var i = 0; i < count; i++) {
-    var random = Math.floor(Math.random() * letters.length);
-    genPassword += letters[random];
+  var pool = lowercaseletters; //lowercase letters are the default starting point, further characters to be specified according to user demands.
+  if (confirm("Would you like your password to contain Upper case letters?")) {
+    pool += uppercaseletters;
   }
-
-  console.log(genPassword);
-
+  if (confirm("Would you like your password to contain numbers?")) {
+    pool += numbers;
+  }
+  if (confirm("Would you like your password to contain special characters?")) {
+    pool += specialchar;
+  }
+  
+  var genPassword = "";
+  for (var i = 0; i < count; i++) {
+    var random = Math.floor(Math.random() * pool.length);
+    genPassword += pool[random];
+  }
   return genPassword;
 }
+
 
 // Write password to the #password input
 function writePassword() {
